10c22016d562ca6926f5b3a071918210f1ae8044
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2023-01-12 22:16:30 +0900

    Optimize 4.5

--- 4.5/main.go
+++ 4.5/main.go
@@ -12,29 +12,81 @@ type TreeNode struct {
 }
 
 func getMaxVal(node *TreeNode) int {
-	if node == nil {
-		return math.MinInt
+	if node.Left == nil && node.Right == nil {
+		return node.Val
 	}
-	left := float64(getMaxVal(node.Left))
-	right := float64(getMaxVal(node.Right))
-	return int(math.Max(math.Max(left, right), float64(node.Val)))
+	if node.Left != nil && node.Right == nil {
+		if node.Val >= node.Left.Val {
+			return node.Val
+		} else {
+			return math.MaxInt
+		}
+	}
+	if node.Left == nil && node.Right != nil {
+		if node.Val <= node.Right.Val {
+			return node.Right.Val
+		} else {
+			return math.MaxInt
+		}
+	}
+	left := getMaxVal(node.Left)
+	right := getMaxVal(node.Right)
+	if left == math.MaxInt || right == math.MaxInt {
+		return math.MaxInt
+	}
+	if left > node.Val || node.Val > right {
+		return math.MaxInt
+	}
+	return int(math.Max(math.Max(float64(left), float64(right)), float64(node.Val)))
 }
 
 func getMinVal(node *TreeNode) int {
-	if node == nil {
-		return math.MaxInt
+	if node.Left == nil && node.Right == nil {
+		return node.Val
 	}
-	left := float64(getMinVal(node.Left))
-	right := float64(getMinVal(node.Right))
-	return int(math.Min(math.Min(left, right), float64(node.Val)))
+	if node.Left != nil && node.Right == nil {
+		if node.Val >= node.Left.Val {
+			return node.Left.Val
+		} else {
+			return math.MinInt
+		}
+	}
+	if node.Left == nil && node.Right != nil {
+		if node.Val <= node.Right.Val {
+			return node.Val
+		} else {
+			return math.MinInt
+		}
+	}
+	left := getMinVal(node.Left)
+	right := getMinVal(node.Right)
+	if left == math.MinInt || right == math.MinInt {
+		return math.MinInt
+	}
+	if left > node.Val || node.Val > right {
+		return math.MinInt
+	}
+	return int(math.Min(math.Min(float64(left), float64(right)), float64(node.Val)))
 }
 
 func isBinarySearchTree(root *TreeNode) bool {
 	if root == nil {
 		return true
 	}
-	leftMax := getMaxVal(root.Left)
-	rightMin := getMinVal(root.Right)
+	leftMax := root.Val - 1
+	if root.Left != nil {
+		leftMax = getMaxVal(root.Left)
+	}
+	if leftMax == math.MaxInt {
+		return false
+	}
+	rightMin := root.Val + 1
+	if root.Right != nil {
+		rightMin = getMinVal(root.Right)
+	}
+	if rightMin == math.MinInt {
+		return false
+	}
 	if leftMax <= root.Val && root.Val <= rightMin {
 		return isBinarySearchTree(root.Left) && isBinarySearchTree(root.Right)
 	} else {
